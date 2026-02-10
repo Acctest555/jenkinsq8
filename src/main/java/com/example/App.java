a058342509045193e3a4572232b8ced588221103
hemanth <swamyco@gmail.com>
2026-02-10 08:58:02 +0530

    changes 1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,7 @@
 package main.java.com.example;
 public class App{
     public static void main(String[] args){
-        System.out.println("Maven project");
+        System.out.println("Maven project version 1.0");
 
     }
 }
